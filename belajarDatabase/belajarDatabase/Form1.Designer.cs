3ec0b1c78a78835c594dfb3ada23cfc1ec0d5a82
fahleviMzi <42181231+fahleviMzi@users.noreply.github.com>
2018-08-08 21:13:22 +0700

    Add files via upload

---
+++ belajarDatabase/belajarDatabase/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace belajarDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_nik = new System.Windows.Forms.TextBox();
+            this.txt_nama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_hp = new System.Windows.Forms.TextBox();
+            this.txt_org = new System.Windows.Forms.TextBox();
+            this.txt_tmpt = new System.Windows.Forms.TextBox();
+            this.txt_rekom = new System.Windows.Forms.TextBox();
+            this.txt_skema = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.data1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(535, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txt_nik
+            // 
+            this.txt_nik.Location = new System.Drawing.Point(109, 191);
+            this.txt_nik.Name = "txt_nik";
+            this.txt_nik.Size = new System.Drawing.Size(100, 20);
+            this.txt_nik.TabIndex = 1;
+            // 
+            // txt_nama
+            // 
+            this.txt_nama.Location = new System.Drawing.Point(109, 218);
+            this.txt_nama.Name = "txt_nama";
+            this.txt_nama.Size = new System.Drawing.Size(100, 20);
+            this.txt_nama.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NIK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nama";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "HP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 438);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Skema Sertifikasi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 384);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Rekomendasi";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 357);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Tempat Ujian";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 411);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Tanggal terbit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tanggal Lahir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Organisasi";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(109, 272);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(100, 20);
+            this.txt_email.TabIndex = 2;
+            // 
+            // txt_hp
+            // 
+            this.txt_hp.Location = new System.Drawing.Point(109, 299);
+            this.txt_hp.Name = "txt_hp";
+            this.txt_hp.Size = new System.Drawing.Size(100, 20);
+            this.txt_hp.TabIndex = 2;
+            // 
+            // txt_org
+            // 
+            this.txt_org.Location = new System.Drawing.Point(109, 326);
+            this.txt_org.Name = "txt_org";
+            this.txt_org.Size = new System.Drawing.Size(100, 20);
+            this.txt_org.TabIndex = 2;
+            // 
+            // txt_tmpt
+            // 
+            this.txt_tmpt.Location = new System.Drawing.Point(109, 353);
+            this.txt_tmpt.Name = "txt_tmpt";
+            this.txt_tmpt.Size = new System.Drawing.Size(100, 20);
+            this.txt_tmpt.TabIndex = 2;
+            // 
+            // txt_rekom
+            // 
+            this.txt_rekom.Location = new System.Drawing.Point(108, 380);
+            this.txt_rekom.Name = "txt_rekom";
+            this.txt_rekom.Size = new System.Drawing.Size(100, 20);
+            this.txt_rekom.TabIndex = 2;
+            // 
+            // txt_skema
+            // 
+            this.txt_skema.Location = new System.Drawing.Point(108, 434);
+            this.txt_skema.Name = "txt_skema";
+            this.txt_skema.Size = new System.Drawing.Size(100, 20);
+            this.txt_skema.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "simpan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 408);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Reload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // data1
+            // 
+            this.data1.Location = new System.Drawing.Point(108, 246);
+            this.data1.Name = "data1";
+            this.data1.Size = new System.Drawing.Size(200, 20);
+            this.data1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 501);
+            this.Controls.Add(this.data1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_skema);
+            this.Controls.Add(this.txt_rekom);
+            this.Controls.Add(this.txt_tmpt);
+            this.Controls.Add(this.txt_org);
+            this.Controls.Add(this.txt_hp);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_nama);
+            this.Controls.Add(this.txt_nik);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_nik;
+        private System.Windows.Forms.TextBox txt_nama;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_hp;
+        private System.Windows.Forms.TextBox txt_org;
+        private System.Windows.Forms.TextBox txt_tmpt;
+        private System.Windows.Forms.TextBox txt_rekom;
+        private System.Windows.Forms.TextBox txt_skema;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker data1;
+    }
+}
+
---
+++ belajarDatabase/belajarDatabase/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace belajarDatabase
+{
+    public partial class Form1 : Form
+    {
+        string koneksi = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:/Users/Embah_Salto/Documents/Visual Studio 2015/Projects/belajarDatabase/belajarDatabase/Mahasiswa.accdb";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sql = "select * from tb_peserta";
+            OleDbConnection con = new OleDbConnection(koneksi);
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter(sql, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "tb_peserta");
+            con.Close();
+            dataGridView1.DataSource = ds.Tables["tb_peserta"].DefaultView;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = string.Format("insert into tb_peserta (nik,nama,tang_lahir,email,hp,organisasi,rekomen,tang_terbit,Kota,Skema) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')",
+                    txt_nik.Text, txt_nama.Text, data1.Text, txt_email.Text, txt_hp.Text, txt_org.Text, txt_rekom.Text,textBox2.Text, txt_tmpt.Text,txt_skema.Text);
+                OleDbConnection conn = new OleDbConnection(koneksi);
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Data Tersimpan", "Pemberitahuan", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (OleDbException salah)
+            {
+                MessageBox.Show(salah.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql1 = "select * from tb_peserta";
+            OleDbConnection con = new OleDbConnection(koneksi);
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter(sql1, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "tb_peserta");
+            con.Close();
+            dataGridView1.DataSource = ds.Tables["tb_peserta"].DefaultView;
+        }
+    }
+}
